0a24be10dc8e90d25bea0e82f6a7572c6a257f75
mspena14 <marlono1naranjo@gmail.com>
2024-04-26 13:41:59 -0500

    Avanzado en clase

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+let menuOpcion = Number(prompt(`
+Por favor ingresa el numero de la opción que deseas: 
+[1] Crear una nueva mascota
+[2] Mostrar mascotas registradas
+[3] Actualizar información mascotas
+[4] Eliminar una mascota
+[5] Salir del programa
+`))
+
+switch (menuOpcion) {   
+    case 1:
+}
